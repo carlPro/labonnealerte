92428b54899c5ab2e96102fe22936457a1d4fe41
Unknown <monnerac194@gmail.com>
2018-01-12 20:12:59 +0100

    Update connecting infos

--- database/Config.php
+++ database/Config.php
@@ -2,6 +2,6 @@
 
 define('__DB_ADRESS__', '127.0.0.1');
 define('__DB_NAME__', 'lba');
-define('__DB_PORT__', '1111');
-define('__DB_USER__', 'fasteel');
-define('__DB_PASSWORD__', 'fasteel');
+define('__DB_PORT__', '3306');
+define('__DB_USER__', 'admin');
+define('__DB_PASSWORD__', 'admin');
